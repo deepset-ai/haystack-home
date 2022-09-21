3b0b8973f6b353279640307d9fbe5abbe652c71a
Carlos Gauci <carlosgauci@gmail.com>
2022-09-21 13:31:29 +0200

    tutorial filters

--- themes/haystack/assets/js/tutorial-filters.js
+++ themes/haystack/assets/js/tutorial-filters.js
@@ -9,8 +9,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Listen for clicks
     filterBtn.addEventListener("click", () => {
-      // If category is enabled, disable it and hide related tutorials
-      if (filterBtn.classList.contains("enabled")) {
+      // Check if any filter is disabled
+      const filterDisabled = Array.from(filterButtons).find((btn) =>
+        btn.classList.contains("disabled")
+      );
+
+      // If all filters are enabled, disable all except clicked
+      if (!filterDisabled) {
+        filterButtons.forEach((btn) => {
+          if (!btn.classList.contains(`category-${category}`)) {
+            btn.classList.remove("enabled");
+            btn.classList.add("disabled");
+          }
+        });
+
+        cards.forEach((card) => {
+          if (!card.classList.contains(`category-${category}`)) {
+            card.style.display = "none";
+          }
+        });
+
+        // if the last filter gets disabled, re-enable all
+      } else if (
+        Array.from(filterButtons).filter((btn) =>
+          btn.classList.contains("enabled")
+        ).length === 1 &&
+        filterBtn.classList.contains("enabled")
+      ) {
+        filterButtons.forEach((btn) => {
+          btn.classList.remove("disabled");
+          btn.classList.add("enabled");
+        });
+
+        cards.forEach((card) => {
+          card.style.display = "flex";
+        });
+      } else if (filterBtn.classList.contains("enabled")) {
+        // Else if category is enabled, disable it and hide related tutorials
         cards.forEach((card) => {
           if (card.classList.contains(`category-${category}`)) {
             card.style.display = "none";

ad41463960746acf6a9f5b359dff5d2f20113341
Carlos Gauci <carlosgauci@gmail.com>
2022-10-17 09:31:28 +0200

    fix toc highlighting

--- themes/haystack/assets/js/table-of-contents.js
+++ themes/haystack/assets/js/table-of-contents.js
@@ -1,15 +1,16 @@
 // Add active class to table of contents links on scroll
 window.addEventListener("DOMContentLoaded", () => {
-  const tocLinks = document.querySelectorAll(`#TableOfContents > ul > li > a`);
-
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       const id = entry.target.getAttribute("id");
       if (entry.intersectionRatio > 0) {
-        tocLinks.forEach((i) => i.classList.remove("active"));
         document
           .querySelector(`#TableOfContents > ul > li > a[href*="${id}"]`)
           .classList.add("active");
+      } else {
+        document
+          .querySelector(`#TableOfContents > ul > li > a[href*="${id}"]`)
+          .classList.remove("active");
       }
     });
   });

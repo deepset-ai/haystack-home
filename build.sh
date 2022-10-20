9ed162f584ae50fdb7efb383f3e63b6fcde5d43a
Massimiliano Pippi <mpippi@gmail.com>
2022-10-20 11:49:03 +0200

    debug

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 rm -rf haystack-tutorials
 git clone --depth 1 https://github.com/deepset-ai/haystack-tutorials.git
+echo "Copying markdown files into ./content/tutorials..."
 cp ./haystack-tutorials/markdowns/* ./content/tutorials
+ls ./content/tutorials
 
 npm install
 hugo -D --gc

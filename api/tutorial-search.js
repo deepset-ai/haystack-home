e51b1c22fa24d08c7379989edb64a4d27e6de6f2
bilgeyucel <bilgeyucel96@gmail.com>
2023-04-27 12:12:05 +0300

    Debug code

--- api/tutorial-search.js
+++ api/tutorial-search.js
@@ -8,9 +8,10 @@ module.exports = async (request, response) => {
         },
         body: JSON.stringify({debug: false, queries: [request.query.query]})
       };
-    
+    console.log("Request is made")
     let temp = await fetch('https://api.cloud.deepset.ai/api/v1/workspaces/test/pipelines/KeywordDocumentSearch_without_metadata_with_highlight/search', options);
-  
+    console.log(temp.status) 
+    console.log(temp.statusText)  
     if (temp.status === 200) {
         const data = await temp.text();
         response.status(200).send(data)

a1b55576a6b61bb7d6200b6ad813bc8244468bec
bilgeyucel <bilgeyucel96@gmail.com>
2023-04-27 12:05:09 +0300

    Add tutorial-search api for requests

---
+++ api/tutorial-search.js
@@ -0,0 +1,22 @@
+module.exports = async (request, response) => {
+    const options = {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json',
+          authorization: `Bearer ${DC_API_KEY}`
+        },
+        body: JSON.stringify({debug: false, queries: [request.query.query]})
+      };
+    
+    let temp = await fetch('https://api.cloud.deepset.ai/api/v1/workspaces/test/pipelines/KeywordDocumentSearch_without_metadata_with_highlight/search', options);
+  
+    if (temp.status === 200) {
+        const data = await temp.text();
+        response.status(200).send(data)
+    } else { 
+        console.error(data) 
+        response.status(temp.status).send(data)
+        // TODO: trace this
+    }
+  }

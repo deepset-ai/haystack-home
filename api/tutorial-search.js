420b18bb7265bd2d6397b29b6111a987c8b421fb
bilgeyucel <bilgeyucel96@gmail.com>
2023-04-27 15:03:34 +0300

    Check node version

--- api/tutorial-search.js
+++ api/tutorial-search.js
@@ -1,4 +1,4 @@
-module.exports = async (request, response) => {
+export default async function tutorialSearch(request, response) {
     const options = {
         method: 'POST',
         headers: {
@@ -8,16 +8,17 @@ module.exports = async (request, response) => {
         },
         body: JSON.stringify({debug: false, queries: [request.query.query]})
       };
+    console.log(process.version)
     console.log("Request is made")
-    let temp = await fetch('https://api.cloud.deepset.ai/api/v1/workspaces/test/pipelines/KeywordDocumentSearch_without_metadata_with_highlight/search', options);
-    console.log(temp.status) 
-    console.log(temp.statusText)  
-    if (temp.status === 200) {
-        const data = await temp.text();
+    const dcResponse = await fetch('https://api.cloud.deepset.ai/api/v1/workspaces/test/pipelines/KeywordDocumentSearch_without_metadata_with_highlight/search', options);
+    console.log(dcResponse.status) 
+    console.log(dcResponse.statusText)  
+    if (dcResponse.status === 200) {
+        const data = await dcResponse.text();
         response.status(200).send(data)
     } else { 
-        console.error(data) 
-        response.status(temp.status).send(data)
+        console.error(dcResponse) 
+        response.status(dcResponse.status).send(dcResponse)
         // TODO: trace this
     }
   }

78d401e478d1ccfa34671c5865bde75ee12f3aed
bilgeyucel <bilgeyucel96@gmail.com>
2023-05-03 18:28:37 +0300

    Improve api calls

--- api/tutorial-search.js
+++ api/tutorial-search.js
@@ -1,24 +1,25 @@
 export default async function tutorialSearch(request, response) {
-    const options = {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          authorization: `Bearer ${process.env.DC_API_KEY}`
-        },
-        body: JSON.stringify({debug: false, queries: [request.query.query]})
-      };
-
-    try {
-      const dcResponse = await fetch(`https://api.cloud.deepset.ai/api/v1/workspaces/${process.env.DC_WORKSPACE_NAME}/pipelines/${process.env.DC_PIPELINE_NAME}/search`, options);
+  const DC_API_URL = "https://api.cloud.deepset.ai/api/v1/workspaces"
+  const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: `Bearer ${process.env.DC_API_KEY}`
+      },
+      body: JSON.stringify({debug: false, queries: [request.query.query]})
+    };
+  
+  try {
+    const dcResponse = await fetch(`${DC_API_URL}/${process.env.DC_WORKSPACE_NAME}/pipelines/${process.env.DC_PIPELINE_NAME}/search`, options);
+    
+    if (dcResponse.status === 200) {
       const result = await dcResponse.text();
-      
-      if (dcResponse.status === 200) {
-        response.status(200).send(result)
-      } else { 
-        response.status(dcResponse.status).send(statusText)
-      }
-    } catch (error) {
-      console.error("Error:", error);
+      response.status(200).send(result)
+    } else { 
+      response.status(dcResponse.status).send(dcResponse.statusText)
     }
+  } catch (error) {
+    console.error("Error:", error);
   }
+}
